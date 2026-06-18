25c62c5b96204bc32ad187894ab8e325b01b9061
naturesindex <whimsystationary@gmail.com>
2026-06-18 14:14:03 -0500

    Update supabase.ts
    fix valid https

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'sb_publishable_3xL_DiEYxsG-hsh3EB8-_w_lTYrqe1n';
-const supabaseKey = 'sb_secret_lRoMWtremcHBnwa3IVQyGA_GWMJcEUs';
+// Replace YOUR_PROJECT_ID with the ID you found in Settings > General
+const supabaseUrl = 'https://uvmqlkmvwjyswwdurgby.supabase.co';
+
+// Use the key labeled 'anon' 'public' from Settings > API
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2bXFsa212d2p5c3d3ZHVyZ2J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3OTMyMDcsImV4cCI6MjA5NzM2OTIwN30.L-9cSewUiuqp88xIlLEUrIJU0-vwiwwh3cz-Jm7yuKU'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
