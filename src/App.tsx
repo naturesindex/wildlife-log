0927bf10e32b39c0050367bf24f1e51bd0444f7c
naturesindex <whimsystationary@gmail.com>
2026-06-30 16:53:33 -0500

    Update App.tsx
    add route to perview page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { GuidePortal } from './components/GuidePortal';
 import { GuestPortal } from './components/GuestPortal';
 import { HomePage } from './components/HomePage';
+import { PreviewPage } from './components/PreviewPage';
 
 
 export default function App() {
@@ -10,9 +11,13 @@ export default function App() {
       <Routes>
         {/* Route 1: The Guide Portal (e.g., /corcovado/guide) */}
         <Route path="/:location/guide" element={<GuidePortal />} />
+        
 
 {/* Route 2: The Guest Portal (Direct Link from Guide) */}
         <Route path="/tour/:tourId" element={<GuestPortal />} />
+
+        {/* The New Preview Page */}
+        <Route path="/preview" element={<PreviewPage />} />
 
        {/* The Main Landing Page (Nature's Index Home) */}
         <Route path="/" element={<HomePage />} />
