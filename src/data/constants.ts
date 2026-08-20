4fa6265289d4b0603d3708a6a65b549b24496407
naturesindex <whimsystationary@gmail.com>
2026-08-20 08:32:27 -0500

    Update constants.ts
    file's exports weren't used anywhere yet, but I corrected them to match reality in case something wires up to them later.

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,18 +1,25 @@
 import { PassportSection } from '../types';
 
 export const SECTION_COLORS: Record<PassportSection, string> = {
-  'Hero Sightings': '#C86A27',
-  'Canopy Crew': '#4A7256',
-  'Sea & Shore': '#3A7CA5',
-  'Forest Floor': '#B04A3C',
+  'Highlights': '#C86A27',
+  'The Canopy Crew': '#4A7256',
+  'Sea and Shore': '#3A7CA5',
+  'The Forest Floor': '#B04A3C',
   'Fascinating Flora': '#8C5170',
+  'Other Notables': '#636B66',
+  'Feathered Royalty': '#C86A27',
+  'Jewels of the Air': '#8C5170',
+  'Canopy Color': '#4A7256',
+  'Forest Songbirds': '#B04A3C',
+  'Woodland Wonders': '#3A7CA5',
+  'Other Discoveries': '#636B66',
 };
 
 export const SECTION_ORDER: PassportSection[] = [
-  'Hero Sightings',
-  'Canopy Crew',
-  'Forest Floor',
-  'Sea & Shore',
+  'Highlights',
+  'The Canopy Crew',
+  'The Forest Floor',
+  'Sea and Shore',
   'Fascinating Flora',
 ];
 
